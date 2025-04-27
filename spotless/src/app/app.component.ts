1b816e04ed35dd2d0ad27f95821cc73239e1c649
Amelia Spell <amspell@ad.unc.edu>
2025-04-27 14:33:22 -0400

    Initial commit (history reset)

---
+++ spotless/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-root',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+
+export class AppComponent {
+  title = 'spotless';
+
+  
+}
