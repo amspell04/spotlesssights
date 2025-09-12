a97596ca346fffd9b83fc3554789e4519b0a42cb
Amelia Spell <amspell1@acg.aaa.com>
2025-09-11 22:53:37 -0400

    more homescreen styling

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,13 +1,16 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 
 @Component({
     selector: 'app-card',
     imports: [],
     templateUrl: './card.component.html',
     styleUrl: './card.component.scss',
+    
 })
 export class CardComponent {
     @Input() title!: string;
     @Input() body!: string;
-   
+    @Input() showIcons!: boolean;
+  
+ 
 }
--- src/app/components/header-nav/header-nav.component.ts
+++ src/app/components/header-nav/header-nav.component.ts
@@ -7,5 +7,7 @@ import { Component } from "@angular/core";
     styleUrl: './header-nav.component.scss',
 })
 export class HeaderNavComponent {
+
+ 
    
 }
