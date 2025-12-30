782360b53e2313bfaba27e47882694763c233131
Amelia Spell <amspell1@acg.aaa.com>
2025-12-29 21:41:21 -0500

    rebuild with routerlinks

--- README.md
+++ README.md
@@ -57,3 +57,10 @@ Angular CLI does not come with an end-to-end testing framework by default. You c
 ## Additional Resources
 
 For more information on using the Angular CLI, including detailed command references, visit the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+
+
+## PRODUCTION
+
+run : ng build --output-path=docs --base-href=/ --configuration=production
+
+then in the docs folder move everything to the root and delete browser
--- src/app/pages/sections/navbar/navbar.component.ts
+++ src/app/pages/sections/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
 import { AfterViewInit, Component, Inject, PLATFORM_ID, signal } from "@angular/core";
 
-import { isPlatformBrowser } from "@angular/common";
-import { Router } from "@angular/router";
+import { CommonModule, isPlatformBrowser } from "@angular/common";
+import { Router, RouterLink, RouterModule } from "@angular/router";
 
 @Component({
     selector: 'app-navbar',
-    imports: [],
+    imports: [RouterLink, RouterModule, CommonModule],
     standalone: true,
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss',
